475bd661c8fd55861abf4ad3b1e56f0533836650
Brandon <brandonlai@berkeley.edu>
2024-12-15 23:47:11 -0800

    fixed home page

--- src/pages/Home1/RecipeFinderSection.tsx
+++ src/pages/Home1/RecipeFinderSection.tsx
@@ -39,98 +39,100 @@ const RecipeFinderSection: React.FC = () => {
 
     return (
         <>
-            <div className="fixed top-0 left-0 right-0 w-full bg-light_green-a700 z-50">
-                <div className="flex justify-center py-[26px] sm:py-5">
-                    <div className="container-xs mt-2 flex flex-row items-center justify-between px-3.5 md:px-5">
-                        <Heading
-                            size="headingmd"
-                            as="h1"
-                            className="font-urbanist text-[40px] font-bold tracking-[1.00px] text-gray-900_01 md:text-[38px] sm:text-[36px]"
-                        >
-                            <Link
-                                to="/"
+            <div className={`flex w-full flex-col gap-10 ${isDarkMode ? 'bg-gray-900' : 'bg-white-a700'}`}>
+                <div className="fixed top-0 left-0 right-0 w-full bg-light_green-a700 z-50">
+                    <div className="flex justify-center py-[26px] sm:py-5">
+                        <div className="container-xs mt-2 flex flex-row items-center justify-between px-3.5 md:px-5">
+                            <Heading
+                                size="headingmd"
+                                as="h1"
+                                className="font-urbanist text-[40px] font-bold tracking-[1.00px] text-gray-900_01 md:text-[38px] sm:text-[36px]"
                             >
-                                <span>Recipe&nbsp;</span>
-                                <span className="font-medium">finder</span>
-                            </Link>
-                        </Heading>
-
-                        <div className="flex gap-4">
-                            
-                            <Link 
-                                to="/search"
-                                className="font-poppins px-4 py-2 bg-white rounded-md hover:bg-gray-300 text-gray-900_01"
-                            >
-                                <img
-                                    src="/images/img_magnifying_glass.svg"
-                                    alt="search"
-                                    className="w-5 h-5"
-                                />
-                            </Link>
-                            <Link 
-                                to="/random"
-                                className="font-poppins px-4 py-2 bg-white rounded-md hover:bg-gray-300"
-                                style={{ color: "#f4f4f4" }}
-                            >
-                                {t('randomRecipe')}
-                            </Link>
-                            {/* when user is logged in, show profile page, otherwise show login page */}
-                            {isLoggedIn ? (
                                 <Link
-                                    to="/profile"
+                                    to="/"
+                                >
+                                    <span>Recipe&nbsp;</span>
+                                    <span className="font-medium">finder</span>
+                                </Link>
+                            </Heading>
+
+                            <div className="flex gap-4">
+                                
+                                <Link 
+                                    to="/search"
                                     className="font-poppins px-4 py-2 bg-white rounded-md hover:bg-gray-300 text-gray-900_01"
                                 >
                                     <img
-                                        src="/images/img_user.svg"
-                                        alt="profile"
+                                        src="/images/img_magnifying_glass.svg"
+                                        alt="search"
                                         className="w-5 h-5"
                                     />
                                 </Link>
-                            ) : (
-                                <Link
-                                    to="/login"
-                                    className="font-poppins px-4 py-2 bg-white rounded-md hover:bg-gray-300 text-gray-900_01"
-                                > 
-                                    <img 
-                                        src="/images/img_user.svg"
-                                        alt="login"
-                                        className="w-5 h-5"
-                                    />
+                                <Link 
+                                    to="/random"
+                                    className="font-poppins px-4 py-2 bg-white rounded-md hover:bg-gray-300"
+                                    style={{ color: "#f4f4f4" }}
+                                >
+                                    {t('randomRecipe')}
                                 </Link>
-                            )}
-                            <select 
-                                onChange={(e) => {
-                                    changeLanguage(e.target.value);
-                                }} 
-                                value={i18n.language}
-                                className="font-poppins px-4 py-2 bg-gray-200 rounded-md w-32"
-                            >
-                                <option value="en">English</option>
-                                <option value="zh-TW">繁體中文</option>
-                                <option value="zh-CN">简体中文</option>
-                                <option value="ms">Melayu</option>
-                            </select>
-                            <button onClick={toggleDarkMode} className="font-poppins px-4 py-2 bg-white rounded-md hover:bg-gray-300">
-                                {isDarkMode ? (
-                                    <img
-                                        src="/images/img_sun.svg"
-                                        alt="light mode"
-                                        className="w-5 h-5"
-                                    ></img>
+                                {/* when user is logged in, show profile page, otherwise show login page */}
+                                {isLoggedIn ? (
+                                    <Link
+                                        to="/profile"
+                                        className="font-poppins px-4 py-2 bg-white rounded-md hover:bg-gray-300 text-gray-900_01"
+                                    >
+                                        <img
+                                            src="/images/img_user.svg"
+                                            alt="profile"
+                                            className="w-5 h-5"
+                                        />
+                                    </Link>
                                 ) : (
-                                    <img
-                                        src="/images/img_moon.svg"
-                                        alt="dark mode"
-                                        className="w-5 h-5"
-                                    ></img>
+                                    <Link
+                                        to="/login"
+                                        className="font-poppins px-4 py-2 bg-white rounded-md hover:bg-gray-300 text-gray-900_01"
+                                    > 
+                                        <img 
+                                            src="/images/img_user.svg"
+                                            alt="login"
+                                            className="w-5 h-5"
+                                        />
+                                    </Link>
                                 )}
-                            </button>
+                                <select 
+                                    onChange={(e) => {
+                                        changeLanguage(e.target.value);
+                                    }} 
+                                    value={i18n.language}
+                                    className="font-poppins px-4 py-2 bg-gray-200 rounded-md w-32"
+                                >
+                                    <option value="en">English</option>
+                                    <option value="zh-TW">繁體中文</option>
+                                    <option value="zh-CN">简体中文</option>
+                                    <option value="ms">Melayu</option>
+                                </select>
+                                <button onClick={toggleDarkMode} className="font-poppins px-4 py-2 bg-white rounded-md hover:bg-gray-300">
+                                    {isDarkMode ? (
+                                        <img
+                                            src="/images/img_sun.svg"
+                                            alt="light mode"
+                                            className="w-5 h-5"
+                                        ></img>
+                                    ) : (
+                                        <img
+                                            src="/images/img_moon.svg"
+                                            alt="dark mode"
+                                            className="w-5 h-5"
+                                        ></img>
+                                    )}
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div className="h-[120px]"></div>
+                <RecipeGallerySection isDarkMode={isDarkMode} />
             </div>
-            <div className="h-[67px]"></div>
-            <RecipeGallerySection isDarkMode={isDarkMode} />
         </>
     );
 };
